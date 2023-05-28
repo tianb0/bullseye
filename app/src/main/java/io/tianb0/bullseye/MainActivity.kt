24c802c1d308288b5f5ed536228a5ce7ba460bbf
Tianbo <tianbo@hey.com>
2023-05-28 10:44:17 -0700

    use view binding

--- app/src/main/java/io/tianb0/bullseye/MainActivity.kt
+++ app/src/main/java/io/tianb0/bullseye/MainActivity.kt
@@ -2,10 +2,15 @@ package io.tianb0.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.tianb0.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
